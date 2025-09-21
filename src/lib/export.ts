e76f314d7d17a49e5d2143f5151f1a90f80f27f1
Sai Tharun Kollampally <stkollamp@gmail.com>
2025-09-21 11:59:40 +0000

    PDF is not complete, it only gave two days of itenerary but user has 7 d

--- src/lib/export.ts
+++ src/lib/export.ts
@@ -4,28 +4,32 @@ import jsPDF from 'jspdf';
 export const exportToPdf = (element: HTMLElement, fileName: string) => {
   html2canvas(element, {
     scale: 2, // Higher scale for better quality
-    useCORS: true, 
+    useCORS: true,
   }).then((canvas) => {
     const imgData = canvas.toDataURL('image/png');
     const pdf = new jsPDF('p', 'mm', 'a4');
+    
     const pdfWidth = pdf.internal.pageSize.getWidth();
     const pdfHeight = pdf.internal.pageSize.getHeight();
+    
     const canvasWidth = canvas.width;
     const canvasHeight = canvas.height;
-    const ratio = canvasWidth / canvasHeight;
-    const width = pdfWidth;
-    const height = width / ratio;
-
-    let position = 0;
-    let heightLeft = (height * pdfWidth) / width;
     
-    pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, (height * pdfWidth) / width );
+    // Calculate the height of the image in the PDF's units
+    const imgHeight = (canvasHeight * pdfWidth) / canvasWidth;
+
+    let heightLeft = imgHeight;
+    let position = 0;
+
+    // Add the first page
+    pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, imgHeight);
     heightLeft -= pdfHeight;
 
-    while (heightLeft >= 0) {
-      position = heightLeft - ((height * pdfWidth) / width);
+    // Add new pages if the content overflows
+    while (heightLeft > 0) {
+      position = -heightLeft;
       pdf.addPage();
-      pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, (height * pdfWidth) / width );
+      pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, imgHeight);
       heightLeft -= pdfHeight;
     }
 
